fa8a8f24fde947f205a4d1a5fdcdfcd3288e02ff
aselengut <aselengut@comcast.net>
2016-11-22 01:16:11 -0500

    Add files via upload

---
+++ ImpressionistView.java
@@ -0,0 +1,322 @@
+package edu.umd.hcil.impressionistpainter434;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.widget.ImageView;
+import java.text.MessageFormat;
+
+
+/**
+ * Created by jon on 3/20/2016.
+ */
+public class ImpressionistView extends View {
+
+    private ImageView _imageView;
+    private Canvas _offScreenCanvas = null;
+    private Bitmap _offScreenBitmap = null;
+    private Paint _paint = new Paint();
+    private Bitmap _imageViewBitmap;
+    private VelocityTracker velocityTracker = null;
+
+    private int _alpha = 150;
+    private int _defaultRadius = 25;
+    private Paint _paintBorder = new Paint();
+    private BrushType _brushType = BrushType.Square;
+
+
+    public ImpressionistView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ImpressionistView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ImpressionistView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    /**
+     * Because we have more than one constructor (i.e., overloaded constructors), we use
+     * a separate initialization method
+     * @param attrs
+     * @param defStyle
+     */
+    private void init(AttributeSet attrs, int defStyle){
+
+        // Set setDrawingCacheEnabled to true to support generating a bitmap copy of the view (for saving)
+        // See: http://developer.android.com/reference/android/view/View.html#setDrawingCacheEnabled(boolean)
+        //      http://developer.android.com/reference/android/view/View.html#getDrawingCache()
+        this.setDrawingCacheEnabled(true);
+
+        _paint.setColor(Color.RED);
+        _paint.setAlpha(_alpha);
+        _paint.setAntiAlias(true);
+        _paint.setStyle(Paint.Style.FILL);
+        _paint.setStrokeWidth(4);
+
+        _paintBorder.setColor(Color.BLACK);
+        _paintBorder.setStrokeWidth(3);
+        _paintBorder.setStyle(Paint.Style.STROKE);
+        _paintBorder.setAlpha(50);
+
+    }
+
+    @Override
+    protected void onSizeChanged (int w, int h, int oldw, int oldh){
+
+        Bitmap bitmap = getDrawingCache();
+        Log.v("onSizeChanged", MessageFormat.format("bitmap={0}, w={1}, h={2}, oldw={3}, oldh={4}", bitmap, w, h, oldw, oldh));
+        if(bitmap != null) {
+            _offScreenBitmap = getDrawingCache().copy(Bitmap.Config.ARGB_8888, true);
+            _offScreenCanvas = new Canvas(_offScreenBitmap);
+        }
+    }
+
+    /**
+     * Sets the ImageView, which hosts the image that we will paint in this view
+     * @param imageView
+     */
+    public void setImageView(ImageView imageView){
+        _imageView = imageView;
+        _imageViewBitmap = _imageView.getDrawingCache();
+    }
+
+    /**
+     * Sets the brush type. Feel free to make your own and completely change my BrushType enum
+     * @param brushType
+     */
+    public void setBrushType(BrushType brushType){
+        _brushType = brushType;
+    }
+
+    public Bitmap get_offScreenBitmap() {
+        return _offScreenBitmap;
+    }
+
+    /**
+     * Clears the painting
+     */
+    public void clearPainting(){
+
+        if(_offScreenCanvas != null) {
+            Paint paint = new Paint();
+            paint.setColor(Color.WHITE);
+            paint.setStyle(Paint.Style.FILL);
+            _offScreenCanvas.drawRect(0, 0, this.getWidth(), this.getHeight(), paint);
+            invalidate();
+        }
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if(_offScreenBitmap != null) {
+            canvas.drawBitmap(_offScreenBitmap, 0, 0, _paint);
+        }
+
+        // Draw the border. Helpful to see the size of the bitmap in the ImageView
+        canvas.drawRect(getBitmapPositionInsideImageView(_imageView), _paintBorder);
+
+        _imageViewBitmap = _imageView.getDrawingCache();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        //TODO
+        //Basically, the way this works is to list for Touch Down and Touch Move events and determine where those
+        //touch locations correspond to the bitmap in the ImageView. You can then grab info about the bitmap--like the pixel color--
+        //at that location
+
+        int curTouchX = (int) motionEvent.getX();
+        int curTouchY = (int) motionEvent.getY();
+        float brushRadius = _defaultRadius;
+        int index = motionEvent.getActionIndex();
+        int pointerId = motionEvent.getPointerId(index);
+
+        //rounds curTouchX and Y so that they can be used
+        curTouchX = round(curTouchX);
+        curTouchY = round(curTouchY);
+
+        int pixel = _imageViewBitmap.getPixel(curTouchX, curTouchY);
+
+        switch(motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if(velocityTracker == null) {
+                    // Retrieve a new VelocityTracker
+                    velocityTracker = VelocityTracker.obtain();
+                }
+                else {
+                    // Reset the velocity tracker
+                    velocityTracker.clear();
+                }
+                // Add user's movement to tracker
+                velocityTracker.addMovement(motionEvent);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int historySize = motionEvent.getHistorySize();
+                _paint.setColor(pixel);
+                _paint.setStrokeWidth(brushRadius);
+                _paint.setAlpha(150);
+
+
+                for (int i = 0; i < historySize; i++) {
+
+                    float touchX = motionEvent.getHistoricalX(i);
+                    float touchY = motionEvent.getHistoricalY(i);
+
+                    if (_brushType == _brushType.Square){
+                        velocityTracker.addMovement(motionEvent);
+                        velocityTracker.computeCurrentVelocity(1000);
+
+                        //changes brush size based on velocity
+                        int x = Math.abs((int)velocityTracker.getXVelocity(pointerId));
+                        int y = Math.abs((int)velocityTracker.getXVelocity(pointerId));
+                        int vel = Math.max(x,y);
+                        brushRadius = getMotionBrushRadius(vel);
+
+                        _offScreenCanvas.drawRect(touchX, touchY, touchX + brushRadius, touchY + brushRadius, _paint);
+                    }
+                    if (_brushType == _brushType.Circle){
+                        velocityTracker.addMovement(motionEvent);
+                        velocityTracker.computeCurrentVelocity(1000);
+
+                        int x = Math.abs((int)velocityTracker.getXVelocity(pointerId));
+                        int y = Math.abs((int)velocityTracker.getXVelocity(pointerId));
+                        int vel = Math.max(x,y);
+                        brushRadius = getMotionBrushRadius(vel);
+
+                        int newPixel = _imageViewBitmap.getPixel(round((int)touchX), round((int)touchY));
+                        _paint.setColor(newPixel);
+                        _paint.setAlpha(150);
+
+                        _offScreenCanvas.drawCircle(touchX, touchY, brushRadius, _paint);
+                    }
+
+                    if(_brushType == _brushType.Line){
+                        brushRadius = 50;
+                        _offScreenCanvas.drawLine(touchX, touchY, touchX + brushRadius, touchY + brushRadius, _paint);
+                    }
+
+                    if(_brushType == _brushType.Splatter){
+
+                        for(i = 0; i< 50; i++){
+                            float randX = (float) randomInRange((int)touchX, (int)touchX + 200);
+                            float randY = (float) randomInRange((int)touchY, (int)touchY + 400);
+
+                            int newPixel = _imageViewBitmap.getPixel(round((int)randX), round((int)randY));
+
+                            _paint.setColor(newPixel);
+                            _paint.setAlpha(150);
+
+                            _offScreenCanvas.drawCircle(randX, randY, brushRadius, _paint);
+                        }
+                    }
+
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                velocityTracker.recycle();
+                velocityTracker = null;
+                break;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    //sets the size of the brush based on velocity
+    private int getMotionBrushRadius(int vel) {
+        int brushRadius = _defaultRadius;
+
+        if(vel <= 400)
+            brushRadius = 30;
+        if(vel > 400 && vel <= 800)
+            brushRadius = 50;
+        if(vel > 800 && vel <= 1000)
+            brushRadius = 70;
+        if(vel > 1000)
+            brushRadius = 80;
+
+
+        return brushRadius;
+    }
+
+
+    /**
+     * This method is useful to determine the bitmap position within the Image View. It's not needed for anything else
+     * Modified from:
+     *  - http://stackoverflow.com/a/15538856
+     *  - http://stackoverflow.com/a/26930938
+     * @param imageView
+     * @return
+     */
+    private static Rect getBitmapPositionInsideImageView(ImageView imageView){
+        Rect rect = new Rect();
+
+        if (imageView == null || imageView.getDrawable() == null) {
+            return rect;
+        }
+
+        // Get image dimensions
+        // Get image matrix values and place them in an array
+        float[] f = new float[9];
+        imageView.getImageMatrix().getValues(f);
+
+        // Extract the scale values using the constants (if aspect ratio maintained, scaleX == scaleY)
+        final float scaleX = f[Matrix.MSCALE_X];
+        final float scaleY = f[Matrix.MSCALE_Y];
+
+        // Get the drawable (could also get the bitmap behind the drawable and getWidth/getHeight)
+        final Drawable d = imageView.getDrawable();
+        final int origW = d.getIntrinsicWidth();
+        final int origH = d.getIntrinsicHeight();
+
+        // Calculate the actual dimensions
+        final int widthActual = Math.round(origW * scaleX);
+        final int heightActual = Math.round(origH * scaleY);
+
+        // Get image position
+        // We assume that the image is centered into ImageView
+        int imgViewW = imageView.getWidth();
+        int imgViewH = imageView.getHeight();
+
+        int top = (int) (imgViewH - heightActual)/2;
+        int left = (int) (imgViewW - widthActual)/2;
+
+        rect.set(left, top, left + widthActual, top + heightActual);
+
+        return rect;
+    }
+
+    //returns a random int in the range specified
+    int randomInRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
+    //rounds the given value so it can be used in the Bitmap
+    int round(int X){
+        if (X < 0) X = 0;
+        if (X>= _imageViewBitmap.getWidth()) X = _imageViewBitmap.getWidth() - 1;
+        return X;
+    }
+}
